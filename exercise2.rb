a52745f1d9b386411ffab3010e8cebb02eebfb3e
JON HOUSE <J_HOUSE974@YAHOO.CA>
2014-10-21 14:35:58 -0400

    Second Exercise

---
+++ exercise2.rb
@@ -0,0 +1,30 @@
+#Exercise 1:How to Calculate a Good Tip for a 55 dollar meal.
+
+bill = 55
+
+tip_amount = (bill * 0.15)
+
+puts "A proper tip would be #{tip_amount}"
+
+
+#Exercise 2
+
+#Adding Strings and Integers
+
+word = "chickenz"
+number = 8.to_s
+
+puts number + word
+
+#Multiplying and String Interpolation
+
+x = 45628
+y = 7839
+sum = x * y
+
+puts "#{x} times #{y} equals #{sum}"
+
+#True or False?
+# it'll be true
+
+puts "(true && false) || (false && true) || !(false && false)"
